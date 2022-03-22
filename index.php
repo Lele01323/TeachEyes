724777066add5c6e902038fa49e3a383d30378cc
Matheus Amorim <matheus.amorim.pinto@gmail.com>
2022-03-22 20:46:59 -0300

    Mudanças nos caminhos para outras páginas

--- index.php
+++ index.php
@@ -9,13 +9,17 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
-    <title>Teste</title>
+    <title>TeachEyes</title>
   </head>
   <body>
     
   <!-- Arquivo de Header -->
   <?php
     include('components/header.php');
+  ?>
+
+  <?php
+    include('pages/home.php');
   ?>
 
   <!-- Arquivo de SideBar -->
