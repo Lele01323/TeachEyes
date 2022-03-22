fdbc9f0ddcbd51446f6ef0b0a45d167f9d97d083
Matheus Amorim <matheus.amorim.pinto@gmail.com>
2022-03-22 20:42:05 -0300

    Criação da página home como teste do conteúdo que será dinâmico

---
+++ pages/home.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="../css/login-style.css">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <title>TeachEyes</title>
+    
+  </head>
+  <body>
+
+  <main class="container">
+    <h2 class="container d-flex justify-content-center">
+        Olá, seja bem-vindo(a) ao seu 
+    </h2>
+    <h2 class="container d-flex justify-content-center">
+        Organizador de Arquivos Acadêmicos
+    </h2>
+    <div class="container estudos">
+        <img src="../img/estudo-de-caso.png" height="70px" width="70px">
+    </div>    
+   </main>
+  
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script src="https://accounts.google.com/gsi/client" async defer></script>
+  </body>
+</html>
+
+<!-- Ícone de estudos -->
+<!-- <a href="https://www.flaticon.com/br/icones-gratis/estudo-de-caso" title="estudo de caso ícones">Estudo de caso ícones criados por SBTS2018 - Flaticon</a> -->
+
+<!-- Ícone de Home -->
+<!-- <a href="https://www.flaticon.com/br/icones-gratis/botao-home" title="botão home ícones">Botão home ícones criados por Kiranshastry - Flaticon</a> -->
+
+<!-- ícone de Home 2 -->
+<!-- <a href="https://www.flaticon.com/br/icones-gratis/casa" title="casa ícones">Casa ícones criados por Freepik - Flaticon</a> -->
+
+<!-- Ícone de PDF -->
+<!-- <a href="https://www.flaticon.com/br/icones-gratis/pdf" title="pdf ícones">Pdf ícones criados por Smashicons - Flaticon</a> -->
+
+<!-- Ícone de Pastas -->
+<!-- <a href="https://www.flaticon.com/br/icones-gratis/pasta" title="pasta ícones">Pasta ícones criados por Payungkead - Flaticon</a> -->
